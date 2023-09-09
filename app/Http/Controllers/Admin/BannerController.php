c922800c4e8fef591b729a3481b75d9567f92443
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-09-10 00:47:17 +0630

    banner index view done

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Banner;
 use Illuminate\Http\Request;
 
 class BannerController extends Controller
 {
-    //
+    public function index(){
+        $banners = Banner::latest()->get();
+        return view('Admin.banners.index', compact('banners'));
+    }
 }
